9ff845ea1f4f6c6d52ba23c93e55d72e0edee1da
Paul Khuong <pvk@pvk.ca>
2020-09-11 14:24:08 -0400

    t/exact_test_sampler_server: gRPC server wrapper for exact_test_sampler
    Run this server on any host, and add an entry to `.sampler_clients.ini`
    to let analyses borrow CPU time from these hosts.
    
    TESTED=locally.

---
+++ t/exact_test_sampler_server.py
@@ -0,0 +1,42 @@
+import argparse
+import concurrent
+import logging
+import grpc
+import os
+
+from exact_test_sampler import ExactTestSampler
+import exact_test_sampler_pb2_grpc as sampler_grpc
+
+
+def serve(port=None):
+    num_requests = 2 * os.cpu_count()
+    server = grpc.server(
+        concurrent.futures.ThreadPoolExecutor(max_workers=2 * num_requests),
+        maximum_concurrent_rpcs=num_requests,
+    )
+    sampler_grpc.add_ExactTestSamplerServicer_to_server(ExactTestSampler(), server)
+
+    listening_address = "localhost:0"
+    if port is not None:
+        listening_address = "localhost:%i" % port
+
+    actual_port = server.add_secure_port(
+        listening_address, grpc.local_server_credentials()
+    )
+    print("ExactTestSampler listening on localhost:%i" % actual_port)
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ExactTestSampler gRPC server.")
+    parser.add_argument(
+        "port",
+        metavar="Port",
+        type=int,
+        nargs="?",
+        help="Port for the gRPC server (picks one if not set).",
+    )
+    args = parser.parse_args()
+    logging.basicConfig()
+    serve(args.port)
